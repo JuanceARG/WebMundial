c5be8fa001ab966ecac92518ae537260ceecd2c4
Juan Garcia <juanmanuelgarcia2@yahoo.com.ar>
2022-10-18 22:48:52 -0300

    Entrega final

--- WebMundial/settings.py
+++ WebMundial/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'WebMundial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [f'{BASE_DIR}/DataMundial/template',f'{BASE_DIR}/DataMundial/template/template_selecciones'],
+        'DIRS': [f'{BASE_DIR}/DataMundial/Template',f'{BASE_DIR}/DataMundial/Template/template_selecciones'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
